3bc137e1a7979928f4add2302f34a093250efa91
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-08-02 18:52:20 -0400

    wip: work on login screen elements

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, Button, Input } from "galio-framework"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.loginText}>Your email</Text>
+      <Input placeholder="e.g. elon@tesla.com" />
+      <Text style={styles.loginText}>Your password</Text>
+      <Input placeholder="Your password here" password={true} viewPass/>
+      <Button round style={styles.signInButton}>Sign in</Button>
+      <View style={styles.linkTextContainer}>
+        <Text style={styles.createAccountText}>Create Account</Text>
+        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30
   },
+  signInButton: {
+    width: "60%",
+    margin: 20
+  },
+  loginText: {
+    textAlign: "left",
+    alignSelf: "stretch",
+    marginTop: 20,
+    marginBottom: 5
+  },
+  linkTextContainer: {
+    flexDirection:'row', 
+    flexWrap:'wrap',
+    marginTop: 20
+  },
+  createAccountText: {
+    marginRight: 20,
+    color: "blue"
+  },
+  forgotPasswordText: {
+    marginLeft: 20,
+    color: "blue"
+  }
 });
