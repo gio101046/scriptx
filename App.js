ba6934279a74e5d5fe839424521f48ed292b3333
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-08-02 19:52:15 -0400

    feat: add login page styling

--- App.js
+++ App.js
@@ -1,35 +1,70 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import { Text, Button, Input } from "galio-framework"
+
+import backgroundImage from "./assets/login-page-background.jpg";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage}>
       <StatusBar style="auto" />
-      <Text style={styles.loginText}>Your email</Text>
-      <Input placeholder="e.g. elon@tesla.com" />
-      <Text style={styles.loginText}>Your password</Text>
-      <Input placeholder="Your password here" password={true} viewPass/>
-      <Button round style={styles.signInButton}>Sign in</Button>
-      <View style={styles.linkTextContainer}>
-        <Text style={styles.createAccountText}>Create Account</Text>
-        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      <View style={styles.screen}>
+          <View style={styles.statusBar}></View>
+          <View style={{height: "20%"}}></View>
+          <View style={styles.modal}>
+            <Text style={styles.loginText}>Your email</Text>
+            <Input placeholder="e.g. elon@tesla.com" />
+            <Text style={styles.loginText}>Your password</Text>
+            <Input placeholder="Your password here" password={true} viewPass/>
+            <Button round style={styles.signInButton} color="#00435D">Sign in</Button>
+            <View style={styles.linkTextContainer}>
+              <Text style={styles.createAccountText}>Create Account</Text>
+              <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+            </View>
+          </View>
+          <View style={{height: "30%"}}></View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
+  },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  statusBar: {
     backgroundColor: "#fff",
+    height: "5%",
+    width: "100%",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+  },
+  modal: {
     alignItems: "center",
     justifyContent: "center",
-    padding: 30
+    height: "45%",
+    width: "85%",
+    padding: 15,
+    backgroundColor: "#fff",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    borderRadius: 10
   },
   signInButton: {
-    width: "60%",
+    width: "100%",
     margin: 20
   },
   loginText: {
---
+++ README.md
@@ -0,0 +1,3 @@
+### Credits
+
+<a href='https://www.freepik.com/vectors/abstract'>Abstract vector created by vector_corp - www.freepik.com</a>
